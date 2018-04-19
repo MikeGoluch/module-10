fc6b209eb0041dad699f49e08b5bdd93fd755967
Michal Goluch <goluch.michal89@gmail.com>
2018-04-19 20:28:39 +0200

    test the new idea for solving the task

--- js/javascript.js
+++ js/javascript.js
@@ -1,38 +1,26 @@
 $(function(){
-	console.log("DOM loaded - you can have fun");
-});
+console.log("DOM loaded - you can have fun");
 //alternative declaration
 
 var ignored = ["first", "second", "fourth"];
 
-console.log(ignored);
-var arrayId = $("span").map(function() {
-    return this.id;
-}).get().join();
+//alternative declaration
 
-var arrayIdString = arrayId.split(",");
-console.log(arrayIdString);
-
-console.log(arrayId);
-
-function diffArray(array1, array2) {
-    var concatArray = array1.concat(array2);
-    return concatArray.filter(function(value, index, array) {
-        if(array.slice(index+1).indexOf(value) === -1 && array.slice(0, index).indexOf(value) === -1) {
-            return value;
-        }
-    });
-}
-console.log(diffArray(ignored, arrayIdString));
-var arrayIdStringWithoutArrayIgnoredId = diffArray(ignored, arrayIdString);
-
-console.log(arrayIdString === arrayIdStringWithoutArrayIgnoredId);
 var span = $("span");
 span.each(function(index, element) {
-    if(arrayIdString === arrayIdStringWithoutArrayIgnoredId) {
-        $(element).css("color", "red");
-    }
+    var spanId = $(this).attr("id");
+    var stringIdArray = spanId.split(",");
+    console.log(stringIdArray);
+        for(var i = 0; i < stringIdArray.length; i++) {
+            for(var j = 0; j < ignored.length; j++) {
+                if(stringIdArray[i] === ignored[j]) {
+                    console.log(stringIdArray[i] === ignored[j]);
+                    $(element).css("color", "red");
+                }
+            }
+        }
 });
+
 
 // $("span").css("color", "red");
 
@@ -45,3 +33,6 @@ paragraphs.each(function(index, element) {
 $("button").click(function() {
     alert($(this).attr("data-tmp"));
 })
+
+});
+
