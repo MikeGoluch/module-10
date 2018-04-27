2011ce0aca5124ed646f7427a6718e4edd91bc5d
Michal Goluch <goluch.michal89@gmail.com>
2018-04-27 20:08:44 +0200

    create new functions for prev and next button

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,38 @@
 $(function(){
 
 var carouselList = $("#carousel ul");
-setInterval(changeSlide, 3000);
+// setInterval(changeSlide, 1000);
 
 
 function moveFirstSlide() {
     var firstItem = carouselList.find("li:first");
     var lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
-    carouselList.css({"marginLeft": 0});
+    carouselList.css({"marginRight": 0});
 }
 
 function changeSlide() {
-    carouselList.animate({"marginLeft": -400}, 3000, moveFirstSlide);
+    carouselList.animate({"marginRight": -400}, 5000, moveFirstSlide);
 }
+
+var previousButton = $("<button>").addClass("previous");
+var previousButtonSymbol = $("<span  class='fas fa-angle-left' aria-hidden='true' aria-hidden='true'></span>");
+previousButton.append(previousButtonSymbol);
+var nextButton = $("<button>").addClass("next");
+var nextButtonSymbol = $("<span  class='fas fa-angle-right' aria-hidden='true' aria-hidden='true'></span>");
+nextButton.append(nextButtonSymbol);
+$("#carousel").append(previousButton);
+$("#carousel").append(nextButton);
+
+$(previousButton).click(function() {
+    carouselList.animate({"marginRight": "+=400px"});
+    moveFirstSlide();
+})
+
+$(nextButton).click(function() {
+    carouselList.animate({"marginRight": "-=400px"});
+    moveFirstSlide();
+})
 
 
 });
