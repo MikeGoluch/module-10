a5db82fcc9de2e24337e9c523243b87b7273e1dc
Michal Goluch <goluch.michal89@gmail.com>
2018-05-01 20:38:44 +0200

    add buttons responsible for chnaging slides

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 $(function(){
 
 var carouselList = $("#carousel ul");
-// setInterval(changeSlide, 1000);
+setInterval(changeSlide, 5000);
 
 
 function moveFirstSlide() {
@@ -11,27 +11,50 @@ function moveFirstSlide() {
     carouselList.css({"marginRight": 0});
 }
 
+//FUNCTION THAT BEGINS THE CAROUSEL
 function changeSlide() {
-    carouselList.animate({"marginRight": -400}, 5000, moveFirstSlide);
+    carouselList.animate({"marginRight": -400}, 0, moveFirstSlide);
 }
 
+//FUNCTION FOR CHANGING SLIDE LEFT
+function previous() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+    carouselList.css({"marginLeft": 0});
+
+}
+
+//FUNCTION FOR CHANGING SLIDE RIGHT
+function next() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({"marginRight": 0});
+
+}
+
+//CREATE BUTTON AND ADD SYMBOLS FOR THEM
 var previousButton = $("<button>").addClass("previous");
-var previousButtonSymbol = $("<span  class='fas fa-angle-left' aria-hidden='true' aria-hidden='true'></span>");
+var previousButtonSymbol = $("<span  class='fas fa-angle-left'></span>");
 previousButton.append(previousButtonSymbol);
+
 var nextButton = $("<button>").addClass("next");
-var nextButtonSymbol = $("<span  class='fas fa-angle-right' aria-hidden='true' aria-hidden='true'></span>");
+var nextButtonSymbol = $("<span  class='fas fa-angle-right'></span>");
 nextButton.append(nextButtonSymbol);
+
+//APPEND BUTTONS TO THE DIV
 $("#carousel").append(previousButton);
 $("#carousel").append(nextButton);
 
+//CHANGING SLIDES LEFT - EVENTS
 $(previousButton).click(function() {
-    carouselList.animate({"marginRight": "+=400px"});
-    moveFirstSlide();
+    carouselList.stop().animate({"marginLeft": "-=400px"}, 0, previous);
 })
 
+//CHANGING SLIDES RIGHT - EVENTS
 $(nextButton).click(function() {
-    carouselList.animate({"marginRight": "-=400px"});
-    moveFirstSlide();
+    carouselList.stop().animate({"marginRight": "-=400px"}, 0, next);
 })
 
 
